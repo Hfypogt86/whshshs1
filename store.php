8f53e1193dd40db1dfd0664b1b087b1562293e00
Hfypogt86 <112763983+Hfypogt86@users.noreply.github.com>
2024-10-25 23:25:58 +0300

    Add files via upload

--- store.php
+++ store.php
@@ -3,9 +3,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $content = $_POST['content'] ?? '';
     if (!empty($content)) {
         $file = 'posts.txt';
-        $current = file_get_contents($file);
-        $current .= htmlspecialchars($content) . "\n";
-        file_put_contents($file, $current);
+        if (is_writable($file)) {
+            $current = file_get_contents($file);
+            $current .= htmlspecialchars($content) . "\n";
+            file_put_contents($file, $current);
+            echo "Success";
+        } else {
+            echo "Error: File not writable";
+        }
+    } else {
+        echo "Error: Content is empty";
     }
+} else {
+    echo "Error: Invalid request method";
 }
 ?>
